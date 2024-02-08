7bf8e8c04a6b713ae5f60d948d1c1e1c9e898d81
Andrey Lopes Cavalcante Mendes <129072446+AndreyCavalcante@users.noreply.github.com>
2024-02-08 10:52:51 -0300

    removendo mensagem

--- js/scriptsobre.js
+++ js/scriptsobre.js
@@ -1,22 +1,3 @@
-var hora = new Date().getHours();
-var msg = document.getElementById("msg");
-
-var mensagem = ""
-
-if(hora >= 5 && hora < 12){
-mensagem = "Olá, Bom dia!"
-}
-else if(hora >= 12 && hora < 18){
-mensagem = "Olá, Boa tarde!"
-}
-else{
-mensagem = "Olá, Boa noite!"
-}
-
-msg.innerHTML = `<h1> ${mensagem} </h1>`
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     var listar = document.getElementById('listar');
     var itens = listar.getElementsByTagName('li');
